9ec34956b349b6651b95a1f53014ec98f9ec130c
大梅 <1048919037@qq.com>
2017-12-29 20:34:40 +0800

    登录界面

---
+++ app/src/main/java/com/example/damei/dormitorychoose/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.damei.dormitorychoose;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by damei on 17/12/29.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login); //加载布局
+    }
+}
+
